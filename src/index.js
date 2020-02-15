7f3f7f4355f951ab11520cf1cb6366c51353b164
Johnson Kit <johnsonkit@users.noreply.github.com>
2020-02-15 13:39:46 +0000

    add config variable for supporting the redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -37,7 +37,10 @@ const reducer = (state = initialState, action) => {
 /**
  * Store
  */
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
